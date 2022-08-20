0da35622b5f476c4d0cc81dae26f667be3798265
mrechavez <milesechavez@gmail.com>
2022-08-19 21:28:53 -0400

    added JS variables

---
+++ main.js
@@ -0,0 +1,15 @@
+//Get all necessary elements from the DOM
+const app = document.querySelector('.weather-app');
+const temp = document.querySelector('.temp');
+const dateOutput = document.querySelector('date');
+const timeOutput = document.querySelector('.time');
+const conditionOutput = document.querySelector('.condition');
+const nameOutput = document.querySelector('.name');
+const icon = document.querySelector('.icon');
+const cloudOutput = document.querySelector('.cloud');
+const humidityOutput = document.querySelector('.humidity');
+const windOutput = document.querySelector('.wind');
+const form = document.querySelector('.locationInput');
+const search = document.querySelector('.search');
+const btn = document.querySelector('.submit');
+const cities = document.querySelector('.city');
